04a9fe79cbdbe8446965859480ade5c2f295660a
Himanshu Mishra <201952215@iiitvadodara.ac.in>
2020-09-20 12:17:41 +0530

    Updates

---
+++ precache-manifest.54ee83237dbf637043e35863129057f7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd18eede8de99c832ebf85cb91e651c2",
+    "url": "/Himanshu44626748/Weather-App/index.html"
+  },
+  {
+    "revision": "6fc95a4f1cb34919a8a0",
+    "url": "/Himanshu44626748/Weather-App/static/css/2.d48897db.chunk.css"
+  },
+  {
+    "revision": "e20ad631da257d6b702e",
+    "url": "/Himanshu44626748/Weather-App/static/css/main.378f620c.chunk.css"
+  },
+  {
+    "revision": "6fc95a4f1cb34919a8a0",
+    "url": "/Himanshu44626748/Weather-App/static/js/2.9e12afb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Himanshu44626748/Weather-App/static/js/2.9e12afb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20ad631da257d6b702e",
+    "url": "/Himanshu44626748/Weather-App/static/js/main.61208b78.chunk.js"
+  },
+  {
+    "revision": "74eee2f15f2e5cd3b821",
+    "url": "/Himanshu44626748/Weather-App/static/js/runtime-main.c62c1007.js"
+  },
+  {
+    "revision": "c67f6de1b8afc128bf2c00195650189a",
+    "url": "/Himanshu44626748/Weather-App/static/media/background.c67f6de1.png"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Himanshu44626748/Weather-App/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Himanshu44626748/Weather-App/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Himanshu44626748/Weather-App/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Himanshu44626748/Weather-App/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/Himanshu44626748/Weather-App/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
